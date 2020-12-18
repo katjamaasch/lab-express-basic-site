fa0722ba53f84ec818745b8d82dc0c0b7e8cba01
Katja Maasch <katja.maasch@gmail.com>
2020-12-18 17:53:03 +0100

    integrated navbar as partial

--- app.js
+++ app.js
@@ -11,14 +11,38 @@ webApplication.set('views', __dirname + '/views');
 webApplication.use(express.static('public'));
 //webApplication.use(express.static('views'));
 
+const data = {
+  name: 'Spock',
+  slogan: 'Welcome Visitors! May you live long and prosper!',
+  heritage: 'mixed Human-Vulcan'
+};
+
+const home = {
+  data,
+  home: true
+};
+
+const about = {
+  data,
+  about: true
+};
+
+const works = {
+  data,
+  works: true
+};
+
+const gallery = {
+  data,
+  gallery: true
+};
+
 webApplication.listen(3000, () => console.log('Server is working'));
 
-webApplication.get('/', (req, res) => res.render('home-page'));
+webApplication.get('/', (req, res) => res.render('home-page', home));
 
-webApplication.get('/about', (req, res) => res.render('about'));
+webApplication.get('/about', (req, res) => res.render('about', about));
 
-webApplication.get('/gallery', (request, response) =>
-  response.render('gallery')
-);
+webApplication.get('/gallery', (req, res) => res.render('gallery', gallery));
 
-webApplication.get('/works', (request, response) => response.render('works'));
+webApplication.get('/works', (req, res) => res.render('works', works));
